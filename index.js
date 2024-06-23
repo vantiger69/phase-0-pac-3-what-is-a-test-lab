20db412e7a23dc08338281afb84ee85a57cb2bdc
Titus kipchirchir <vantiger817@tgmail.com>
2024-06-23 08:03:42 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const { expect } = require("chai");
+
 const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message }
